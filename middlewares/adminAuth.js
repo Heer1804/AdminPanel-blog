2922d810342ac68d8a8f1217c2e52c79775b626a
Heer1804 <parikhheer8@gmail.com>
2024-11-18 17:36:20 +0530

    first commit

---
+++ middlewares/adminAuth.js
@@ -0,0 +1,9 @@
+module.exports.adminAuth = (req, res, next) => {
+    let { adminId } = req.cookies;
+    if (adminId) {
+      return next();
+    } else {
+      return res.redirect("/login");
+    }
+  };
+  
